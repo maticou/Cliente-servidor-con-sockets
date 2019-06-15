89cdb9525829c0feb107a99bc7a0adb328781349
maticou <maticou123@gmail.com>
2019-06-14 20:26:47 -0400

    Clase cliente hecha

--- Cliente/src/cliente/Cliente.java
+++ Cliente/src/cliente/Cliente.java
@@ -5,17 +5,37 @@
  */
 package cliente;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  *
  * @author mati_
  */
 public class Cliente {
 
+    public static String HOST = "127.0.0.1";
+    public static int PUERTO = 5000;
+    public static DataInputStream entrada;
+    public static DataOutputStream salida;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+            try (Socket socket = new Socket(HOST, PUERTO)) {
+                entrada = new DataInputStream(socket.getInputStream());
+                salida = new DataOutputStream(socket.getOutputStream());
+                salida.writeUTF("Soy el cliente y quiero conectarme. Déjame entrar!!!!");
+                
+                String mensaje = entrada.readUTF();                
+                System.out.println(mensaje);
+            }catch(IOException ex){
+                System.out.println(ex);
+            }
     }
-    
 }
